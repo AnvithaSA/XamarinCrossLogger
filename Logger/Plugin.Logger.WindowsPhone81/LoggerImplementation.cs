0f1cb9b3cd3c180746db3c5fd9d9d0c8ee99fd9b
AnvithaSA <31839657+AnvithaSA@users.noreply.github.com>
2017-09-11 12:08:11 +0530

    Add files via upload

---
+++ Logger/Plugin.Logger.WindowsPhone81/LoggerImplementation.cs
@@ -0,0 +1,41 @@
+using Plugin.Logger.Abstractions;
+using System;
+
+namespace Plugin.Logger
+{
+    /// <summary>
+    /// Implementation for Logger
+    /// </summary>
+    public class LoggerImplementation : LoggerBase
+    {
+        public override bool AppendToFile(string filename, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetFileSizeKb(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetNextAvailableFileName(string logFileNameBase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string LoadFromFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SaveToFile(string filename, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
