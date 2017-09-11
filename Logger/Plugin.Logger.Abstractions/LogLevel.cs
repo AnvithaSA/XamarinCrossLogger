6e182879efc943bdbc0330abfda02f27704e2d54
AnvithaSA <31839657+AnvithaSA@users.noreply.github.com>
2017-09-11 11:57:21 +0530

    Add files via upload

---
+++ Logger/Plugin.Logger.Abstractions/LogLevel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Logger.Abstractions
+{
+    /// <summary>
+    /// Contains levels of Logs
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// logs all records
+        /// </summary>
+        All = 0,
+        /// <summary>
+        ///  logs debug
+        /// </summary>
+        Debug = 1,
+        /// <summary>
+        /// logs info, warning and error records
+        /// </summary>
+        Info = 2,
+        /// <summary>
+        /// logs warning and error records
+        /// </summary>
+        Warning = 3,
+        /// <summary>
+        /// logs only error records
+        /// </summary>
+        Error = 4
+    }
+}
